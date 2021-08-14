65a83fd3b3668724bcc8551b6603dfcd8daf45b8
ps <ananthups666@gmail.com>
2021-08-14 09:38:57 +0530

    Added dictionary to fruit project

--- fruit.py
+++ fruit.py
@@ -1,76 +1,110 @@
-print("FRUIT SHOP")
-print("-----------")
-
-fruit = []
-
+fruits = {}
 while True:
-	print("1. Add Fruit")
-	print("2. Delete Fruit by name")
-	print("3. Search Fruit by name and rate")
-	print("4. Change Fruit name and rate")
-	print("5. Display Fruits list") 
-	print("6. exit")
+    print("1.Add fruit")
+    print("2.Delete fruit by name")
+    print("3.Search fruit by name and rate")
+    print("4.Change fruit name and rate")
+    print("5.Add to cart")
+    print("6.Display")
+    print("7.Exit")
+    choice = int(input("Enter your choice :"))
 
-	ch = int(input("Enter your choice : "))
+    if choice == 1:
+        fruit_id = input("\tEnter fruit_id:")
+        if fruit_id not in fruits.keys():
 
-	if ch == 1:
-		f = input("Enter the fruit name : ")
-		r = int(input("Enter the rate : "))
-		fruit.append([f,r])
-		print(fruit)
+            name = input("\tEnter fruit name :")
+            rate = input("\tenter the rate of this fruit :")
+            imported_from = input("\tEnter imported_from :")
+            import_date = input("\tEnter import_date (in DD/MM/YYYY) :")
+            buy_price = int(input("\tEnter buy_price:"))
+            temp ={
+                "name":name,
+                "rate":rate,
+                "imported_from":imported_from,
+                "import_date":import_date,
+                "buy_price":buy_price
+                }
+            fruits[fruit_id] = temp        
 
-	elif ch == 2:
-		#Delete  fruit
-		sub = []
-		print(fruit)
-		print("Choose name from this:")
-		name = input("Enter fruit name to delete : ")
-		rate = int(input("Enter the rate : "))
-		sub.append(name)
-		sub.append(rate)
-		for i in fruit :
-			if i == sub:
-				fruit.remove(i) 
+    elif choice == 2:
+        fruit_id = input("\tEnter fruit_id:")
+        name = input("\tEnter fruit name :")
+        flag = False
+        if fruit_id in fruits.keys():
+            for fruitid in fruits.values():
+                if fruitid["name"] == name :
+                    flag = True
+        else:
+            print("wrong id ")
+        if flag == True:
+            del fruits[fruit_id]
+    
+    elif choice == 3:
+        print("1 .search by name ")
+        print("2 .search by rate")
+        choice1 = int(input("Enter your choice :"))
 
-	elif ch == 3:
-		#Search fruit
-		sub = []
-		name = input("Enter name of fruit you want to search : ")
-		rate = int(input("Enter the rate you want to search : "))
-		sub.append(name)
-		sub.append(rate)
-		for i in fruit:
-			if i == sub:
-				print(name + "," + rate, " is in the list")
-			else:
-				print(name + "," + rate, "is not in the list")
+        if choice1 == 1:
+            name = input("Enter name you want to search :")
+            for i in fruits.values():
+                if i["name"] == name:
+                    print(f"\t{i['name']} | {i['rate']} | {i['imported_from']} | {i['import_date']} | {i['buy_price']} ")
+                    flag = True
+                    break
+            if flag == False :
+                print("\t Not Found")
+        elif choice1 == 2 :
+            rate = input("Enter rate you want search : ")
+            for i in fruits.values():
+                if i["rate"] == rate:
+                    print(f"\t{i['name']} | {i['rate']} | {i['imported_from']} | {i['import_date']} | {i['buy_price']} ")
+                    flag = True
+                    break
+            if flag == False :
+                print("\tNot Found")
 
-	elif ch == 4:
-		#Change a fruit and rate in the list
-		sub = []
-		name = input("Enter the fruit name : ")
-		rate = int(input("Enter the rate : "))
-		sub.append(name)
-		sub.append(rate)
-		index =	fruit.index(sub)
-		new_name = input("Enter new fruit name : ")
-		new_rate = int(input("Enter the new rate : "))
-		sub1 = []
-		sub1.append(new_name)
-		sub1.append(new_rate)
-		fruit[index] = sub1
-		print(fruit)
-		
-	elif ch == 5:
-		#Display fruits
-		if len(fruit) != 0:
-			for i in range(0,len(fruit)):
-				print(i+1,".",fruit[i][0],fruit[i][1])
-		else:
-			print("Item not in the list")
-	elif ch == 6:
-		#Exit
-		break;
-		
-	else:
-		break
+
+    elif choice == 4:
+        print("1  .Change fruit name")
+        print("2  .change rate")
+        choice = int(input("\tenter your choice : "))
+        if choice == 1:
+            fruit_id = input("\tEnter fruit_id :")
+            if fruit_id not in fruits.keys():
+                print("\twrong fruit_id")
+            else:
+                fruits[fruit_id]['name'] = input("\tEnter new name")  
+        elif choice == 2:
+            fruit_id = input("\tEnter fruit id :")
+            if fruit_id not in fruits.keys():
+                print("\twrong fruit_id")
+            else:
+                fruits[fruit_id]['rate'] = input("\tEnter new rate")  
+
+    elif choice == 5:
+
+        cart_list = []
+        for id,fruitname in fruits.items():
+            cart_list.append(id + ":" + fruitname["name"])
+        for i in cart_list:
+            print(i)
+        print("you can select from above list to add cart ")
+        cart_l=[] 
+        print("\tyou can enter fruit id to add cart")  
+        cart_inp = int(input("enter the fruit_id to add cart : "))
+        cart_l.append(cart_inp)
+        print("\t your cart fruit ids",cart_inp)
+
+
+
+    elif choice == 6:
+        for frtid,fruit in fruits.items():
+            print(f"\t{frtid} | {fruit['name']} | {fruit['rate']} | {fruit['imported_from']} | {fruit['import_date']} | {fruit['buy_price']} ")
+     
+              
+
+    elif choice == 7:
+        break;
+    else:
+        print("invalid choice")
